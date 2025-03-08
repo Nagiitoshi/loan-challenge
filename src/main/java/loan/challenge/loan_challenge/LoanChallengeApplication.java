1ec083b891ac0fd4f2a0ca439f0067b043e4fea4
Nagi <nagiitoshi@gmail.com>
2025-03-08 16:52:44 -0300

    first commit

---
+++ src/main/java/loan/challenge/loan_challenge/LoanChallengeApplication.java
@@ -0,0 +1,13 @@
+package loan.challenge.loan_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loan/challenge/loan_challenge/LoanChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package loan.challenge.loan_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
